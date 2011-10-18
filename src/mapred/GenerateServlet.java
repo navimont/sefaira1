6118159fae9f23bfdcbfe6c9abf0da9b7d0a9f0c
Stefan W <sw1@monton.de>
2011-10-18 11:57:15 -0400

    Generation of testdata seems to work

---
+++ src/mapred/GenerateServlet.java
@@ -0,0 +1,111 @@
+package mapred;
+
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.DatastoreServiceFactory;
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.Text;
+import com.google.appengine.api.datastore.Transaction;
+import com.google.appengine.api.users.User;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.gson.Gson;
+
+import java.lang.Integer;
+import java.lang.Long;
+import java.lang.Float;
+import java.lang.NumberFormatException;
+import java.io.IOException;
+import java.util.List;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.logging.Logger;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GenerateServlet extends HttpServlet {
+    private static final Logger log =
+            Logger.getLogger(GenerateServlet.class.getName());
+
+    private static Map<String,Float> generateData(int len) {
+    	Map<String, Float> datalist = new HashMap<String, Float>();
+    	Random rand = new Random();
+    	
+    	int i;
+    	for (i=0; i<len; i++) {
+    		datalist.put(String.format("VAR%03d", i), (rand.nextFloat()-0.5F)*100.0F);
+    	}
+    	
+    	return datalist;
+    }
+    
+    public void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws IOException {
+    	Gson gson = new Gson();
+        Integer number = 0;
+        
+        // get the number of new data sets to create in the DB
+        try {
+        	number = Integer.valueOf(req.getParameter("number"));
+        } catch (NumberFormatException e) {}
+        // not more than 100 at a time
+        number = number > 100 ? 100 : number;
+        
+        // read current number of entities in datastore
+        DatastoreService datastore =  DatastoreServiceFactory.getDatastoreService();
+    	Entity counter;
+    	Long fileIndex;
+		Key counterKey = KeyFactory.createKey("Counter", "filecounter");
+		try {
+			counter = datastore.get(counterKey);
+	    	fileIndex = (Long) counter.getProperty("total_files");			
+    	} catch (EntityNotFoundException e) {
+    		fileIndex = 0L;
+    	}         
+                
+        if (number > 0) {
+            Transaction txn = datastore.beginTransaction();
+            
+        	try {
+            	List<Entity> jsondataList = new LinkedList<Entity>();
+            	try {
+            		counter = datastore.get(counterKey);
+                	fileIndex = (Long) counter.getProperty("total_files");
+            	} catch (EntityNotFoundException e) {
+            		// first usage of the storage. Create the element.
+            		counter = new Entity(counterKey);
+            		fileIndex = 0L;
+            	}
+            	
+            	while (number > 0) {	        	
+    		        fileIndex++;
+    		        number--;
+            		// counter is a parent, needed for write in transaction. The file index is part of the key
+            		Key jsondataKey = KeyFactory.createKey(counterKey, "jsondata", fileIndex);
+    		        Entity jsondata = new Entity(jsondataKey);
+    		        // generate a json dataset with key-value pairs
+    		        Text jsontext = new Text(gson.toJson(generateData(300)));
+    		        jsondata.setProperty("data", jsontext);
+    		        jsondataList.add(jsondata);
+            	}
+                counter.setUnindexedProperty("total_files", fileIndex);
+                log.info(jsondataList.size() + " datasets ready for commit. Last file index is " + fileIndex);
+            	datastore.put(counter);
+                datastore.put(jsondataList);
+                txn.commit();
+        	} finally {
+        		if (txn.isActive()) {
+        			txn.rollback();
+        		}
+        	}
+        }
+
+        resp.sendRedirect("/generate.jsp");
+    }
+}
---
+++ war/generate.jsp
@@ -0,0 +1,36 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page import="com.google.appengine.api.datastore.DatastoreServiceFactory" %>
+<%@ page import="com.google.appengine.api.datastore.DatastoreService" %>
+<%@ page import="com.google.appengine.api.datastore.Entity" %>
+<%@ page import="com.google.appengine.api.datastore.EntityNotFoundException" %>
+<%@ page import="com.google.appengine.api.datastore.Key" %>
+<%@ page import="com.google.appengine.api.datastore.KeyFactory" %>
+
+<html>
+  <body>
+
+    <h1>Generate random data sets</h1>
+    <p>Each dataset will be stored in JSON format and contain 300 variables and their values. Variables are named "VAR000".."VAR200". Values are random and lay between -50.0 and 50.0</p>   
+<%
+    DatastoreService datastore =  DatastoreServiceFactory.getDatastoreService();
+	Entity counter;
+	Long fileIndex;
+	Key counterKey = KeyFactory.createKey("Counter", "filecounter");
+	try {
+		counter = datastore.get(counterKey);
+    	fileIndex = (Long) counter.getProperty("total_files");			
+	} catch (EntityNotFoundException e) {
+		fileIndex = 0L;
+	}         
+%>
+	
+  <%= fileIndex %> datasets currently in the database;
+
+  <form action="/generate" method="get">
+  	Choose how many random datasets to generate (max. 100): 
+    <div><input type="text" name="number"/></div>
+    <div><input type="submit" value="Generate..." /></div>
+  </form>
+  
+  </body>
+</html>
